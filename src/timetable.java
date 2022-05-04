cf0e7f163ccdce9f01b42fd39f27d1ec6190f84e
nishavaland <nishavaland92@gmail.com>
2022-05-04 22:57:07 +0100

    timetable

---
+++ src/timetable.java
@@ -0,0 +1,12 @@
+public class timetable{
+    public static void main(String [] args) {
+        for (int i = 2; i <=10; i++) {
+            for (int j =0; j <=10; j++ ) {
+                System.out.println(i + " * " +j+ " = " +i*j);
+            }
+        }
+    }
+}
+
+
+
